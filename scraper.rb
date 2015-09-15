8878a17a49f52803144ce9e29deb11fdedfd6321
Henare Degan <henare.degan@gmail.com>
2015-09-15 15:48:18 +1000

    Add todo

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,8 @@ agent = Mechanize.new
 vote_event_url = "http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_golos?g_id=3106"
 vote_event_page = agent.get(vote_event_url)
 
+# TODO: Save vote_event data
+
 # Vote results by faction
 vote_event_page.search("#01 ul.fr > li").each do |faction|
   faction_name = faction.at(:b).inner_text
