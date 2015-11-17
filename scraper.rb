8ce6192ce06747a24fdc20fe54d8dc13173a3d7c
Henare Degan <henare.degan@gmail.com>
2015-11-17 11:37:22 +1100

    Make faction name search fuzzier
    The vote pages seem to use shorter names in some cases. This catches
    those cases and I think it's safe to do.

--- scraper.rb
+++ scraper.rb
@@ -80,7 +80,7 @@ end
 def faction_name_to_id(name)
   @factions ||= morph_scraper_query("openaustralia/ukraine_verkhovna_rada_deputies",
                                     "SELECT DISTINCT COALESCE(faction_id, 'party/Позафракційні') AS faction_id, COALESCE(faction, 'Позафракційні') AS faction FROM 'data'")
-  if faction = @factions.find { |f| f["faction"] == name }
+  if faction = @factions.find { |f| f["faction"].include? name }
     faction["faction_id"]
   else
     raise "Faction ID not found for: #{name}"
